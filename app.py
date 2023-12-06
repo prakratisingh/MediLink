c9e2262b51ef0b2233bf3c0b2277356a55e46184
Prakrati Singh <500082638@stu.upes.ac.in>
2023-12-06 22:31:22 +0530

    comments added to app.py

--- app.py
+++ app.py
@@ -1,34 +1,43 @@
+# importing flask modules to handle flask operations 
 from flask import Flask,redirect, render_template, request, url_for, session
+# to manage sessions in the project
 from flask_session import Session
 import os
+# for image manipulation
 from PIL import Image
+# importing the model and processor
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 from werkzeug.utils import secure_filename
+# importing our own modules
 from data import DataBase
 from bchain import MedBlockchain
 from datetime import date
 from transformers import utils
 utils.logging.set_verbosity_error()
 
-# Initialize Flask app
+# Initializing the Flask app
 app = Flask(__name__)
+# creating the session
 app.config['SESSION_PERMANENT'] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# setting the path of the working directory
 path = " "
+# loading the processor and model
 processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-handwritten')
 model = VisionEncoderDecoderModel.from_pretrained('model')
 
-
+# creating the object of database and blockchain module
 db = DataBase()
 medchain = MedBlockchain()
 
-
+# API for login page
 @app.route('/')
 def login():    
     return render_template('login.html')
 
+# API to authenticate the login
 @app.route('/login_authenticate',methods=['POST'])
 def login_authenticate():
     if request.method == "POST":
@@ -40,6 +49,7 @@ def login_authenticate():
         else:
             return render_template('faillogin.html')
 
+# API for registration authentication
 @app.route('/register_authenticate',methods=['POST','GET'])
 def register_authenticate():
     if request.method == 'POST':
@@ -52,24 +62,26 @@ def register_authenticate():
             session["name"] = request.form.get("name")
             return redirect(url_for('home'))
 
-
+# API for registration page
 @app.route('/register')
 def register():
     return render_template('register.html')
 
+# function to return template of home page
 @app.route('/home',methods=['POST','GET'])
 def home():
     if not session.get("name"):
         return redirect("/")
     return render_template('home.html')
 
+# function to upload the image data
 @app.route('/upload',methods=['POST','GET'])
 def upload():
     if not session.get("name"):
         return redirect("/")
     return render_template("upload.html")
 
-
+# API to generate text from the uploaded image
 @app.route('/process', methods=['POST'])
 def process_image():
     if not session.get("name"):
@@ -88,7 +100,8 @@ def process_image():
         return render_template('saveData.html',generatedtext=generated_text)
     else:
         return render_template('upload.html')
-    
+
+# API to save the data in the blockchain   
 @app.route('/savedata',methods=['POST'])
 def savedata():
     if not session.get("name"):
@@ -102,6 +115,7 @@ def savedata():
 
     return render_template('successsave.html')
 
+# function to check the integrity of user and display his data
 @app.route('/showdata',methods=['GET'])
 def showdata():
     username = session['name']
@@ -115,6 +129,7 @@ def showdata():
         else:
             return render_template('notsecure.html')
 
+# function to logout from the session
 @app.route('/logout')
 def logout():
     if not session.get("name"):
